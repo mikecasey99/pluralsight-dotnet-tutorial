c087435ff37228743e002be9db9266a5b6e2ef6b
mike casey <mikecasey1120@gmail.com>
2025-07-24 22:46:53 -0400

    finished sections 1-4

---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,38 @@
+// Loads configuration from appsettings.json, environment variables, etc.
+// Also sets up Kestrel (default web server) and optionally IIS integration (on Windows)
+// wwwroot folder will be the default folder to hold static content.
+// Also gives us access to the services collection which helps us register with dependency injection
+using BethanysPieShop.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// One instance per web request
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+// Bringing in framework services that enable MVC.
+builder.Services.AddControllersWithViews();
+
+
+
+var app = builder.Build();
+// After builder.Build() is executed its when we can bring in our middleware components.
+
+// Middleware components will be expressed typically with app.method()
+// Example of middleware that will check wwwroot default folder.
+
+app.UseStaticFiles();
+
+// Show us private information that may help debugging.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+// Set defaults in MVC to route to our views.
+app.MapDefaultControllerRoute();
+
+// Starts our application.
+app.Run();
+
+
